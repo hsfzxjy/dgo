5602b49e5cdb4f49b14bceea20b80c423e446ddb
hsfzxjy <hsfzxjy@gmail.com>
2022-11-28 23:14:02 +0800

    dgo-gen: NewExports() accepts physical path instead of Go module name

--- dgo-gen/internal/exports.go
+++ dgo-gen/internal/exports.go
@@ -1,12 +1,18 @@
 package internal
 
 import (
+	"io"
+	"os"
+	"path"
+
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/collector"
+	"github.com/hsfzxjy/dgo/dgo-gen/internal/exception"
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/exported"
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/generator"
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/generator/gogen"
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/generator/irgen"
 	"github.com/hsfzxjy/dgo/dgo-gen/internal/interfaces"
+	"golang.org/x/mod/modfile"
 	"golang.org/x/tools/go/packages"
 )
 
@@ -15,17 +21,28 @@ type Exports struct {
 	Context  *collector.Context
 }
 
-func NewExports(pkgPath string) *Exports {
+func NewExports(goModDir string) *Exports {
+	goModPath := path.Join(goModDir, "go.mod")
+	goModFile, err := os.Open(goModPath)
+	exception.Die(err)
+	defer goModFile.Close()
+	goModContent, err := io.ReadAll(goModFile)
+	exception.Die(err)
+	mod, err := modfile.Parse(goModPath, goModContent, nil)
+	exception.Die(err)
+
+	modName := mod.Module.Mod.Path
+
 	ex := &Exports{Packages: make(map[string]*exported.Package)}
 	ex.Context = collector.NewContext(ex)
-	ex.ParsePackages(pkgPath, false)
+	ex.ParsePackages(modName, false)
 	return ex
 }
 
 func (ex *Exports) Package(name string) interfaces.Package { return ex.Packages[name] }
 
 func (ex *Exports) ParsePackages(pkgPath string, isDep bool) {
-	pkgs := LoadPackages(pkgPath)
+	pkgs := LoadPackages(pkgPath + "/...")
 	missingPkgs := []*packages.Package{}
 	for _, pkg := range pkgs {
 		if _, ok := ex.Packages[pkg.PkgPath]; !ok {
