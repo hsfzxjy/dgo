e536973c5b1d0184edbe2c529f403ca3217d199f
hsfzxjy <hsfzxjy@gmail.com>
2022-12-08 17:41:23 +0800

    dart: add interfaces to build object by type or typeId

--- dart/lib/dgo.dart
+++ dart/lib/dgo.dart
@@ -34,6 +34,12 @@ class _Dgo extends DgoPortLike with _PortMixin {
 
   Future<DgoPort> newPort(DynamicLibrary dylib, {String name = 'custom'}) =>
       DgoPort._build('dgo:dart:$name', dylib, isDefault: false);
+
+  DgoObject Function(int typeId, Iterator args) get buildObjectById =>
+      _buildObjectById;
+
+  T Function<T extends DgoObject>(Iterator args) get buildObject =>
+      _buildObject;
 }
 
 final dgo = _Dgo();
