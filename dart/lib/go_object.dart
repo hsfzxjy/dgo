ea772a58bd28e8df9f0118b0e59a8f333c0ce01c
hsfzxjy <hsfzxjy@gmail.com>
2022-12-08 17:40:20 +0800

    dart: add interfaces for registering generated types

--- dart/lib/go_object.dart
+++ dart/lib/go_object.dart
@@ -1,1 +1,22 @@
 part of dgo;
+
+abstract class DgoObject {
+  const DgoObject();
+
+  @protected
+  int $dgoStore(List<dynamic> args, int index);
+
+  int get $dgoGoSize;
+}
+
+late final DgoObject Function(int typeId, Iterator args) _buildObjectById;
+late final T Function<T extends DgoObject>(Iterator args) _buildObject;
+
+@experimental
+void registerTypes(
+  DgoObject Function(int, Iterator) buildObjectById,
+  T Function<T extends DgoObject>(Iterator) buildObject,
+) {
+  _buildObjectById = buildObjectById;
+  _buildObject = buildObject;
+}
